169976b84736a4570a4538b015b7f4ecd29483a9
Georg <georgestoneix@gmail.com>
2024-08-26 10:33:09 +0600

    Настроил админку

--- homework70/webapp/admin.py
+++ homework70/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Article, Comment
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title',  'created_at']
+    list_display_links = ['id', 'title']
+    # list_filter = ['author']
+    search_fields = ['title', 'content']
+    fields = ['title',  'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Comment)
