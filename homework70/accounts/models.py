74802c3f8989eacc8925ea9f5732e41b4f0eca8b
Georg <georgestoneix@gmail.com>
2024-08-26 17:03:20 +0600

    Обновил модельку

--- homework70/accounts/models.py
+++ homework70/accounts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+
 from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
     birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
-    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    avatar = models.ImageField(null=True, blank=True, upload_to='avatars', verbose_name='Аватар',)
 
     def __str__(self):
         return self.user.get_full_name() + "'s Profile"
